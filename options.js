00e27e002e04ddcb04aaf97968fb6a55f6a9039a
KamilMatejuk <kamilmatejuk@gmail.com>
2025-06-02 10:40:04 +0200

    fix loading defaults and reseting

--- options.js
+++ options.js
@@ -10,7 +10,13 @@ function showExample(options) {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
+    // load options
     chrome.storage.sync.get(Object.keys(DEFAULT_OPTIONS), (savedOptions) => {
+        // load default options if not set
+        savedOptions = Object.fromEntries(
+            Object.keys(DEFAULT_OPTIONS).map(id => [id, savedOptions[id] || DEFAULT_OPTIONS[id]])
+        )
+        chrome.storage.sync.set(savedOptions);
         // on change
         Object.entries(DEFAULT_OPTIONS).forEach(o => {
             const [id, defaultValue] = o;
@@ -28,16 +34,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
         // reset
         document.getElementById("reset").addEventListener("click", () => {
-            Object.entries(DEFAULT_OPTIONS).forEach(o => {
-                const [id, defaultValue] = o;
-                const option = document.getElementById(id);
-                if (id.startsWith("hide")) {
-                    option.checked = defaultValue == "true";
-                } else {
-                    option.value = defaultValue;
-                }
-            });
-            chrome.storage.sync.set(DEFAULT_OPTIONS, () => alert('Options saved!'));
+            chrome.storage.local.clear(() => alert('Options reset to default'));
+            chrome.storage.sync.clear();
         })
 
         // save
@@ -47,7 +45,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     [id, id.startsWith("hide") ? document.getElementById(id).checked : document.getElementById(id).value]
                 )
             )
-            chrome.storage.sync.set(newValues, () => alert('Options saved!'));
+            chrome.storage.sync.set(newValues, () => alert('Options saved'));
         })
             
         // example
